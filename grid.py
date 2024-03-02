71fa1e6ba25e56972b234a9887f0bf0335e55af7
whoagi <whoagir@yandex.ru>
2024-03-02 16:46:40 +0400

    check grid

--- grid.py
+++ grid.py
@@ -9,6 +9,7 @@ from perlin_noise import PerlinNoise
 class Grid(object):
     ''' ВАЖНО ПОНИМАТЬ, ЧТО СЕТКА ИМЕЕТ ВИД dict[(local_x, local_y)] = ((global_x, global_y), height)
     key = (local_x, local_y) values = ((global_x, global_y), height) '''
+
     def __init__(self):  # создаем переменную "сетки", указываем её тип (словарь)
         self.grid = dict()
 
@@ -27,21 +28,24 @@ class Grid(object):
                 color = (abs(inf[1] * 100), abs(inf[1] * 100), abs(inf[1] * 100))
                 if inf[1]:
                     pygame.draw.circle(screen, color, inf[0], 3)
-                    rect = pygame.Rect(inf[0][0]-3, inf[0][1]+3, cell_sise, cell_sise)
+                    rect = pygame.Rect(inf[0][0] - 3, inf[0][1] + 3, cell_sise, cell_sise)
                     pygame.draw.rect(screen, color, rect)
 
     def completion(self, heights: list):  # генерируем сетку, где ключ у нас локальные координаты,
         # а значения это глобальные координаты и высота (переводим локальные координаты в глобальные + получаем высоты)
         for i in range(numbers_width_grid + 1):
             for j in range(numbers_height_grid + 1):
-                self.grid[(i, j)] = (x_global_coord_grid + width * i / numbers_width_grid - width / (numbers_width_grid * 2),
-                                     y_global_coord_grid + height * j / numbers_height_grid - height / (numbers_height_grid * 2)), \
+                self.grid[(i, j)] = (x_global_coord_grid + width * i / numbers_width_grid - width / (
+                            numbers_width_grid * 2),
+                                     y_global_coord_grid + height * j / numbers_height_grid - height / (
+                                                 numbers_height_grid * 2)), \
                                     heights[i][j]
 
     def globloc(self):
         for i in range(width + 1):
             for j in range(height + 1):
-                self.grid[(i, j)] =
+                # self.grid[(i, j)] =
+                pass
 
     def generate_perlin_noise(self, seed):  # метод генерации высот
         # генерация основного шума и параметризация
